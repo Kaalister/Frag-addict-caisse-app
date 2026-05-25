43a76390d9887e7bfef6f65ca127067d5c52e50a
Kaalister <samuel.caillot@epitech.eu>
2026-05-25 18:55:11 +0200

    add missing files

--- FragsAddicts/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:frags_addicts_caisse/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('money formats French euro display', () {
-    expect(money(12.5), '12,50 €');
-  });
-}
